4e5fe477555b61e81b867834809c592f68433f4c
matheusmpz <matheus.p.soares242@gmail.com>
2024-11-19 16:56:43 -0300

    first-commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                'azul': '#061F3E',
+                'laranja': '#E74A40',
+                'branco': '#eeeeee',
+                'cinza': '#2f2f2f',
+              },
+        },
+    },
+    plugins: [],
+};
